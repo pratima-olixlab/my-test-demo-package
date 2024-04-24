0ea7ff412791ea971e8ca55cd034d31086d0a59c
pratima-olixlab <159017898+pratima-olixlab@users.noreply.github.com>
2024-04-24 17:08:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
-// import './index.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';  
-// import App from './App';
-
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById('root')
-// );
-
-
 import React from 'react';
 import { createRoot } from 'react-dom/client'; // Import createRoot from 'react-dom/client' instead of ReactDOM
 import { BrowserRouter } from 'react-router-dom';
